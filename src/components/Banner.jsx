3e7af3041564ead3f99653e75e5e4e4527f27d35
ADILAHAMEDK <adilahamedk5842@gmail.com>
2025-01-08 14:16:42 +0530

    machine task

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import banner from '../assets/images/girlBanner.jpg'
+
+const Banner = () => {
+  return (
+    <div>
+        <img src={banner} alt="img" className='w-full' />
+    </div>
+  )
+}
+
+export default Banner
